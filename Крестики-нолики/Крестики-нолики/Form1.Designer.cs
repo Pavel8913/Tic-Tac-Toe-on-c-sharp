d7d602255762c59283c83ee4956a919197881e87
Pavel <143266883+Pavel8913@users.noreply.github.com>
2024-01-19 21:21:44 +0700

    Add files via upload

---
+++ Крестики-нолики/Крестики-нолики/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Крестики_нолики
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt_restart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.Color.LightBlue;
+            this.bt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt1.Location = new System.Drawing.Point(0, 0);
+            this.bt1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 61);
+            this.bt1.TabIndex = 0;
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            this.bt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt2
+            // 
+            this.bt2.BackColor = System.Drawing.Color.LightBlue;
+            this.bt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt2.Location = new System.Drawing.Point(0, 0);
+            this.bt2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(75, 61);
+            this.bt2.TabIndex = 1;
+            this.bt2.UseVisualStyleBackColor = false;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            this.bt2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt3
+            // 
+            this.bt3.BackColor = System.Drawing.Color.LightBlue;
+            this.bt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt3.Location = new System.Drawing.Point(271, 33);
+            this.bt3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(75, 61);
+            this.bt3.TabIndex = 2;
+            this.bt3.UseVisualStyleBackColor = false;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            this.bt3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt4
+            // 
+            this.bt4.BackColor = System.Drawing.Color.LightBlue;
+            this.bt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt4.Location = new System.Drawing.Point(29, 116);
+            this.bt4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(75, 60);
+            this.bt4.TabIndex = 3;
+            this.bt4.UseVisualStyleBackColor = false;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            this.bt4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt5
+            // 
+            this.bt5.BackColor = System.Drawing.Color.LightBlue;
+            this.bt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt5.Location = new System.Drawing.Point(145, 116);
+            this.bt5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(75, 60);
+            this.bt5.TabIndex = 4;
+            this.bt5.UseVisualStyleBackColor = false;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            this.bt5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt6
+            // 
+            this.bt6.BackColor = System.Drawing.Color.LightBlue;
+            this.bt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt6.Location = new System.Drawing.Point(271, 116);
+            this.bt6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(75, 60);
+            this.bt6.TabIndex = 5;
+            this.bt6.UseVisualStyleBackColor = false;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            this.bt6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt7
+            // 
+            this.bt7.BackColor = System.Drawing.Color.LightBlue;
+            this.bt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt7.Location = new System.Drawing.Point(29, 199);
+            this.bt7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(75, 61);
+            this.bt7.TabIndex = 6;
+            this.bt7.UseVisualStyleBackColor = false;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            this.bt7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt8
+            // 
+            this.bt8.BackColor = System.Drawing.Color.LightBlue;
+            this.bt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt8.Location = new System.Drawing.Point(145, 199);
+            this.bt8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(75, 61);
+            this.bt8.TabIndex = 7;
+            this.bt8.UseVisualStyleBackColor = false;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            this.bt8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt9
+            // 
+            this.bt9.BackColor = System.Drawing.Color.LightBlue;
+            this.bt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt9.Location = new System.Drawing.Point(271, 199);
+            this.bt9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(75, 61);
+            this.bt9.TabIndex = 8;
+            this.bt9.UseVisualStyleBackColor = false;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            this.bt9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bt1_KeyPress);
+            // 
+            // bt_restart
+            // 
+            this.bt_restart.Location = new System.Drawing.Point(380, 116);
+            this.bt_restart.Name = "bt_restart";
+            this.bt_restart.Size = new System.Drawing.Size(75, 60);
+            this.bt_restart.TabIndex = 9;
+            this.bt_restart.Text = "Начать заново";
+            this.bt_restart.UseVisualStyleBackColor = true;
+            this.bt_restart.Click += new System.EventHandler(this.bt_restart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.bt1);
+            this.panel1.Location = new System.Drawing.Point(29, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 61);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.bt2);
+            this.panel2.Location = new System.Drawing.Point(145, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(75, 61);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(271, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(75, 61);
+            this.panel3.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(29, 116);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(75, 60);
+            this.panel4.TabIndex = 13;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(145, 116);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(75, 60);
+            this.panel5.TabIndex = 14;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Black;
+            this.panel6.Location = new System.Drawing.Point(271, 116);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(75, 60);
+            this.panel6.TabIndex = 15;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Black;
+            this.panel7.Location = new System.Drawing.Point(29, 199);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(75, 61);
+            this.panel7.TabIndex = 16;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Black;
+            this.panel8.Location = new System.Drawing.Point(145, 199);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(75, 61);
+            this.panel8.TabIndex = 17;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Black;
+            this.panel9.Location = new System.Drawing.Point(271, 199);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(75, 61);
+            this.panel9.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(355, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 60);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Победили \r\nкрестики\r\n(раз):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(355, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 60);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Победили \r\nнолики\r\n(раз):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(485, 303);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.bt_restart);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "крестики-нолики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt_restart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Крестики-нолики/Крестики-нолики/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Крестики_нолики
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool metka = false;
+        int cl = 1;
+        int нолики=0, крестики=0;
+        private void bt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void bt_restart_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+        
+        private void reset()
+        {
+            cl = 1;
+            bt1.Enabled = true;
+            bt2.Enabled = true;
+            bt3.Enabled = true;
+            bt4.Enabled = true;
+            bt5.Enabled = true;
+            bt6.Enabled = true;
+            bt7.Enabled = true;
+            bt8.Enabled = true;
+            bt9.Enabled = true;
+            bt1.Text = "";
+            bt2.Text = "";
+            bt3.Text = "";
+            bt4.Text = "";
+            bt5.Text = "";
+            bt6.Text = "";
+            bt7.Text = "";
+            bt8.Text = "";
+            bt9.Text = "";
+        }
+     
+        #region проверка
+        private void proverka()
+        {
+            //Первая горизонталь
+            if (bt1.Text == "0" && bt2.Text == "0" && bt3.Text == "0")
+            {
+                MessageBox.Show("Победили нолики!");
+                reset();
+                нолики++;
+                label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                
+
+            }
+            if (bt1.Text == "Х" && bt2.Text == "Х" && bt3.Text == "Х")
+            {
+                MessageBox.Show("Победили крестики!");
+                reset();
+                крестики++;
+                label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                
+            }
+            //вторая горизонталь
+            if (bt4.Text == "0" && bt5.Text == "0" && bt6.Text == "0")
+            {
+                MessageBox.Show("Победили нолики!");
+                reset();
+                нолики++;
+                label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+
+            }
+            if (bt4.Text == "Х" && bt5.Text == "Х" && bt6.Text == "Х")
+            {
+                MessageBox.Show("Победили крестики!");
+                reset();
+                крестики++;
+                label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+            }
+            //Третья горизонталь
+            if (bt7.Text == "0" && bt8.Text == "0" && bt9.Text == "0")
+            {
+                MessageBox.Show("Победили нолики!");
+                reset();
+                нолики++;
+                label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+            }
+            if (bt7.Text == "Х" && bt8.Text == "Х" && bt9.Text == "Х")
+            {
+                MessageBox.Show("Победили крестики!");
+                reset();
+                крестики++;
+                label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+            }
+            //Первый крест
+            if (bt1.Text == "0" && bt5.Text == "0" && bt9.Text == "0")
+            {
+                MessageBox.Show("Победили нолики!");
+                reset();
+                нолики++;
+                label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+
+            }
+            if (bt1.Text == "Х" && bt5.Text == "Х" && bt9.Text == "Х")
+            {
+                MessageBox.Show("Победили крестики!");
+                reset();
+                крестики++;
+                label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+            }
+            //Второй крест
+                if (bt3.Text == "0" && bt5.Text == "0" && bt6.Text == "0")
+                {
+                    MessageBox.Show("Победили нолики!");
+                    reset();
+                    нолики++;
+                    label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+                if (bt3.Text == "Х" && bt5.Text == "Х" && bt6.Text == "Х")
+                {
+                    MessageBox.Show("Победили крестики!");
+                    reset();
+                    крестики++;
+                    label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                }
+                if (bt1.Text == "0" && bt4.Text == "0" && bt7.Text == "0")
+                {
+                    MessageBox.Show("Победили нолики!");
+                    reset();
+                    нолики++;
+                    label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+                if (bt1.Text == "Х" && bt4.Text == "Х" && bt7.Text == "Х")
+                {
+                    MessageBox.Show("Победили крестики!");
+                    reset();
+                    крестики++;
+                    label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                }
+                if (bt1.Text == "0" && bt4.Text == "0" && bt7.Text == "0")
+                {
+                    MessageBox.Show("Победили нолики!");
+                    reset();
+                    нолики++;
+                    label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+                if (bt2.Text == "Х" && bt5.Text == "Х" && bt8.Text == "Х")
+                {
+                    MessageBox.Show("Победили крестики!");
+                    reset();
+                    крестики++;
+                    label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                }
+                if (bt2.Text == "0" && bt5.Text == "0" && bt8.Text == "0")
+                {
+                    MessageBox.Show("Победили нолики!");
+                    reset();
+                    нолики++;
+                    label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+                if (bt3.Text == "Х" && bt6.Text == "Х" && bt9.Text == "Х")
+                {
+                    MessageBox.Show("Победили крестики!");
+                    reset();
+                    крестики++;
+                    label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                }
+                if (bt3.Text == "0" && bt6.Text == "0" && bt9.Text == "0")
+                {
+                    MessageBox.Show("Победили нолики!");
+                    reset();
+                    нолики++;
+                    label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+                if (bt7.Text == "Х" && bt5.Text == "Х" && bt3.Text == "Х")
+                {
+                    MessageBox.Show("Победили крестики!");
+                    reset();
+                    крестики++;
+                    label1.Text = "Победили \r\nкрестики\r\n(раз):" + крестики;
+                }
+                if(bt7.Text == "0" && bt5.Text == "0" && bt3.Text == "0")
+                {
+                MessageBox.Show("Победили нолики!");
+                reset();
+                нолики++;
+                label2.Text = "Победили \r\nнолики\r\n(раз):" + нолики;
+                }
+            #endregion
+        #region кнопки
+        }
+        private void bt1_Click(object sender, EventArgs e)
+        {                      
+            if(cl%2==0)
+            {
+                bt1.Text = "0";
+                //bt2.ForeColor= Color.White;
+            }
+            else
+            {
+                bt1.Text = "Х"; 
+                //bt1.BackColor=Color.Transparent;
+                //bt1.FlatStyle = FlatStyle.Popup;
+                //bt1.ForeColor = Color.Black;
+               
+            }
+            cl++;
+            bt1.Enabled = false;
+            proverka();
+           
+        }
+       
+        private void bt2_Click(object sender, EventArgs e)
+        {            
+            if (cl % 2 == 0)
+            {
+                bt2.Text = "0";
+            }
+            else
+            {
+                bt2.Text = "Х";
+            }
+            cl++;
+            bt2.Enabled = false;
+            proverka();
+        }
+
+       
+        private void bt7_Click(object sender, EventArgs e)
+        {
+
+            if (cl % 2 == 0)
+            {
+                bt7.Text = "0";
+            }
+            else
+            {
+                bt7.Text = "Х";
+            }
+            cl++;
+            bt7.Enabled = false;
+            proverka();
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt3.Text = "0";
+            }
+            else
+            {
+                bt3.Text = "Х";
+            }
+            cl++;
+            bt3.Enabled = false;
+            proverka();
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt4.Text = "0";
+            }
+            else
+            {
+                bt4.Text = "Х";
+            }
+            cl++;
+            bt4.Enabled = false;
+            proverka();
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt5.Text = "0";
+            }
+            else
+            {
+                bt5.Text = "Х";
+            }
+            cl++;
+            bt5.Enabled = false;
+            proverka();
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt6.Text = "0";
+            }
+            else
+            {
+                bt6.Text = "Х";
+            }
+            cl++;
+            bt6.Enabled = false;
+            proverka();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt8.Text = "0";
+            }
+            else
+            {
+                bt8.Text = "Х";
+            }
+            cl++;
+            bt8.Enabled = false;
+            proverka();
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            if (cl % 2 == 0)
+            {
+                bt9.Text = "0";
+            }
+            else
+            {
+                bt9.Text = "Х";
+            }
+            cl++;
+            bt9.Enabled = false;
+            proverka();
+
+        }
+        #endregion
+
+
+    }
+}
